168161eed9765f0262b4c15cfd9a947915bf41be
phtre57 <phtre57@gmail.com>
2024-02-13 15:44:41 -0500

    CORS light

--- frontend/src/views/MonteCarloSimulationsView.vue
+++ frontend/src/views/MonteCarloSimulationsView.vue
@@ -18,7 +18,6 @@ export default {
         const response = await fetch('http://127.0.0.1:8000/portfolios/monte-carlo-sims?stocks=AAPL&stocks=GOOG&stocks=TSLA&weights=0.3&weights=0.4&weights=0.3&initial_value=10000');
         const data = await response.json();
         this.data = data;
-        console.log(data); // Log data to console
       } catch (error) {
         console.error('Error fetching data:', error);
       }
--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
 from backend.src.infra.api.fastapi.routers import PortfolioRouter
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "*",
+]
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(PortfolioRouter.router)
 
